03b72c74d1b209339fe45406d1c54ff7352dc1af
Notessawy <elessawy@aucegypt.edu>
2023-06-15 14:52:38 +0200

    message we are the best

---
+++ function
@@ -0,0 +1,3 @@
+int max(int a, int b);
+int knapSack(int W, int wt[], int val[], int n);
+
---
+++ function1.cpp
@@ -0,0 +1,9 @@
+#include "function2.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	checkSolution();
+}
+
